4c6f3c9c43dc64d12e25a28081e4b1fc11830186
Jordi Prats <jprats@systemadmin.es>
2016-01-18 12:50:13 +0100

    afegit path al class systemd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
 #
 class systemd inherits systemd::params {
 
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
+
   exec { 'systemctl reload':
     command     => 'systemctl daemon-reload',
     refreshonly => true,
