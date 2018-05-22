0f8d2e2a73d02e923b8820ce59f4a86259acc91b
Jordi Prats <jordi.prats@gmail.com>
2018-05-22 17:22:13 +0200

    typo

--- manifests/timesyncd/service.pp
+++ manifests/timesyncd/service.pp
@@ -4,13 +4,13 @@ class systemd::timesyncd::service inherits systemd::timesyncd {
   $is_docker_container=str2bool($is_docker_container_var)
 
   if( $is_docker_container==false or
-      $systemd::resolved::manage_docker_service)
+      $systemd::timesyncd::manage_docker_service)
   {
-    if($systemd::resolved::manage_service)
+    if($systemd::timesyncd::manage_service)
     {
       service { 'systemd-timesyncd':
-        ensure     => $systemd::resolved::service_ensure,
-        enable     => $systemd::resolved::service_enable,
+        ensure     => $systemd::timesyncd::service_ensure,
+        enable     => $systemd::timesyncd::service_enable,
         hasstatus  => true,
         hasrestart => true,
       }
