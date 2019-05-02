bdc34f811c784f30bfff30830bbebd429e842efc
Vincent <34089151+ArVincentr@users.noreply.github.com>
2019-05-02 14:08:11 +0200

    moved to its own namespace

---
+++ manifests/target.pp
@@ -0,0 +1,26 @@
+define systemd::target ( 
+                        $targetname   = $name, 
+                        $description  = undef,
+                        $allowisolate = 'no',
+                                  
+  ) {
+
+
+  if versioncmp($::puppetversion, '4.0.0') >= 0
+    {
+      contain ::systemd
+    }
+    else
+    {
+      include ::systemd
+    }
+    
+   file { "/etc/systemd/system/${targetname}.target":
+     ensure  => 'present',
+     owner   => 'root',
+     group   => 'root',
+     mode    => '0644',
+     content => template("${module_name}/target.erb"),
+     notify  => Exec['systemctl daemon-reload'],
+  }
+}
