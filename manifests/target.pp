8815bcdeb7c518620d1faa8a974746d33217eaa0
Vincent <34089151+ArVincentr@users.noreply.github.com>
2019-05-02 14:41:48 +0200

    rework indentation

--- manifests/target.pp
+++ manifests/target.pp
@@ -15,12 +15,12 @@ define systemd::target (
       include ::systemd
     }
     
-   file { "/etc/systemd/system/${targetname}.target":
-     ensure  => 'present',
-     owner   => 'root',
-     group   => 'root',
-     mode    => '0644',
-     content => template("${module_name}/target.erb"),
-     notify  => Exec['systemctl daemon-reload'],
+  file { "/etc/systemd/system/${targetname}.target":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/target.erb"),
+    notify  => Exec['systemctl daemon-reload'],
   }
 }
