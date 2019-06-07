054fc3af34676980192c80e6b3de38b026c0d83f
Jordi Prats <jordi.prats@gmail.com>
2019-06-07 12:35:00 +0200

    systemd::sysvwrapper configurable restart

--- manifests/sysvwrapper.pp
+++ manifests/sysvwrapper.pp
@@ -3,6 +3,7 @@ define systemd::sysvwrapper (
                               $servicename          = $name,
                               $check_time           = '10m',
                               $wait_time_on_startup = '1s',
+                              $restart              = 'no',
                             ) {
 
   if versioncmp($::puppetversion, '4.0.0') >= 0
@@ -38,7 +39,7 @@ define systemd::sysvwrapper (
                       "${initscript}.sysvwrapper.status",
                       ] ],
     forking   => true,
-    restart   => 'no',
+    restart   => $restart,
     pid_file  => "/var/run/${servicename}.sysvwrapper.pid",
   }
 
