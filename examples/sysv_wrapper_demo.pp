31c3fe0b3bc61f77485e814510150f70625920e7
Jordi Prats <jordi.prats@gmail.com>
2019-06-07 13:12:30 +0200

    demo

--- examples/sysv_wrapper_demo.pp
+++ examples/sysv_wrapper_demo.pp
@@ -1,6 +1,4 @@
 systemd::sysvwrapper { "dockercontainer_${container_id}":
   initscript => "/etc/init.d/dockercontainer_${container_id}",
   restart    => 'always',
-  notify     => Service["dockercontainer_${container_id}"],
-  before     => Service["dockercontainer_${container_id}"],
 }
