4eedeeb3c2ef0c8618281c77089495545875c4c6
Jordi Prats <jordi.prats@gmail.com>
2019-10-01 14:15:35 +0200

    automount

---
+++ manifests/automount.pp
@@ -0,0 +1,80 @@
+#
+# [Unit]
+# Description=Things devices
+# After=network.target
+#
+# [Mount]
+# What=172.16.24.192:/mnt/things
+# Where=/mnt/things
+# Type=nfs
+# Options=_netdev,auto
+#
+# [Install]
+# WantedBy=multi-user.target
+#
+define systemd::automount (
+                            $what,
+                            $where            = $name,
+                            $type             = undef,
+                            $options          = [],
+                            $directory_mode   = '0755',
+                            $timeout_idle_sec = '0',
+                            # install
+                            $also             = [],
+                            $default_instance = undef,
+                            $service_alias    = [],
+                            $wantedby         = [ 'multi-user.target' ],
+                            $requiredby       = [],
+                            # unit
+                            $description      = undef,
+                            $documentation    = undef,
+                            $wants            = [],
+                            $after            = undef,
+                            $after_units      = [],
+                            $before_units     = [],
+                            $requires         = [],
+                            $binds_to         = [],
+                            $conflicts        = [],
+                            $on_failure       = [],
+                            $partof           = undef,
+                            $allow_isolate    = undef,
+                            # global
+                            $ensure           = 'present',
+                          ) {
+  if versioncmp($::puppetversion, '4.0.0') >= 0
+  {
+    contain ::systemd
+  }
+  else
+  {
+    include ::systemd
+  }
+
+  $mount_name = regsubst(regsubst($where, '/', '-', 'G'), '^-', '', '')
+
+  concat { "/etc/systemd/system/${mount_name}.automount":
+    ensure => $ensure,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    notify => Exec['systemctl daemon-reload'],
+  }
+
+  concat::fragment { "automount ${mount_name} unit":
+    target  => "/etc/systemd/system/${mount_name}.automount",
+    order   => '00',
+    content => template("${module_name}/section/unit.erb"),
+  }
+
+  concat::fragment { "automount ${mount_name} install":
+    target  => "/etc/systemd/system/${mount_name}.automount",
+    order   => '01',
+    content => template("${module_name}/section/install.erb"),
+  }
+
+  concat::fragment { "automount ${mount_name} automount":
+    target  => "/etc/systemd/system/${mount_name}.automount",
+    order   => '02',
+    content => template("${module_name}/section/automount.erb"),
+  }
+}
